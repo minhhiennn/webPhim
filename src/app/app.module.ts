4ca3eca3d892831109f0d71478a84967e12c2449
shegga <shegga9x@gmail.com>
2021-10-04 11:12:26 +0700

    update config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
+import { WatchingComponent } from './Components/watching/watching.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WebphimdetailsComponent } from './Components/webphimdetails/webphimdetails.component';
+import { HeaderComponent } from './Common/header/header.component';
+import { FooterComponent } from './Common/footer/footer.component';
+
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WebphimdetailsComponent
+    WebphimdetailsComponent,
+    WatchingComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
