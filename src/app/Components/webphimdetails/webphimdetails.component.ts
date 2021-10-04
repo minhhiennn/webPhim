dfd9b1e5cdbfa9010580d3d7d3ee17c276831d5a
minhhiennn <18130076@st.hcmuaf.edu.vn>
2021-10-04 10:28:17 +0700

    boostrap

---
+++ src/app/Components/webphimdetails/webphimdetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-webphimdetails',
+  templateUrl: './webphimdetails.component.html',
+  styleUrls: ['./webphimdetails.component.scss']
+})
+export class WebphimdetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
