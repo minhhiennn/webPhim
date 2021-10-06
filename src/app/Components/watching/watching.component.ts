7a8637d61d6e39663aa7455778ff687b2c20067e
shegga <shegga9x@gmail.com>
2021-10-06 11:20:59 +0700

    ok

--- src/app/Components/watching/watching.component.ts
+++ src/app/Components/watching/watching.component.ts
@@ -1,3 +1,4 @@
+import { OwlOptions } from 'ngx-owl-carousel-o';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WatchingComponent implements OnInit {
 
+  customOptions: OwlOptions = {
+
+    loop: true,
+    mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    autoplay: true,
+    nav: false,
+    navSpeed: 700,
+    responsive: {
+      0: {
+        items: 1
+      },
+      200: {
+        items: 2
+      },
+      400: {
+        items: 3
+      },
+      705: {
+        items: 4
+      },
+      800: {
+        items: 5
+      }
+    }
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
 }
